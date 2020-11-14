6b2d4e891e75132b6152329b74a32e9258d1f309
Aceki <aceki320@gmail.com>
2020-11-14 14:39:59 +0500

    feat: createMark errors handling!

--- controllers/api/graph.js
+++ controllers/api/graph.js
@@ -38,12 +38,39 @@ module.exports.createMark = (req, res) => apiTools.parameterizedHandler(["type"]
 */
 
 module.exports.createMark = apiTools.parameterizedHandler(["data"], async function body(obj, req, res) {
-    let data = JSON.parse(obj.data);
+    let data;
+
+    try {
+        data = JSON.parse(obj.data);
+    } 
+    catch(err) {
+        apiTools.sendReport(res, new ApiReport("error", 999, "JSON format error!"));
+        return;
+    }
+    
+    if(data.type == undefined) {
+        apiTools.sendReport(res, new ApiReport("error", 999, "Wrong mark type!"));
+        return;
+    }
+
+    if(data.properties == undefined) {
+        apiTools.sendReport(res, new ApiReport("error", 999, "Properties is undefined!"));
+        return;
+    }
+
     let result = await db.graph.createNode(["Mark"], {type: data.type});
     let markID = result.records[0]._fields[0].low;
     for(let i = 0; i < data.properties.length; i++) {
-        let propertyID = (await db.graph.createNode(["Property"], data.properties[i])).records[0]._fields[0].low;
-        await db.graph.createRelation(markID, propertyID, "property");
+        let obj;
+        try {
+            obj = await apiTools.checkFilling(data.properties[i], ["propertyName", "require", "default"]);
+        }
+        catch(requireParam) {
+            apiTools.sendReport(res, new ApiReport("error", 999, requireParam));
+            return;
+        }
+        let propertyID = (await db.graph.createNode(["Property"], obj)).records[0]._fields[0].low;
+        await db.graph.createRelation(markID, propertyID, "property")
     }
     apiTools.sendReport(res, new ApiReport("ok", 0, "Successful!"));
 }); 
