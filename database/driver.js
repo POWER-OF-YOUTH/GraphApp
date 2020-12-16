1ce525650931620334703cc10f6d62c5f2fd3fec
Aceki <aceki320@gmail.com>
2020-12-16 13:03:03 +0500

    Refactoring

--- database/driver.js
+++ database/driver.js
@@ -1,6 +1,6 @@
 const dbConfig = require('../configs/database.json');
 
 const neo4j = require('neo4j-driver');
-const driver = neo4j.driver(dbConfig.host, neo4j.auth.basic(dbConfig.user, dbConfig.password));
+const driver = neo4j.driver(dbConfig.host, neo4j.auth.basic(dbConfig.user, dbConfig.password), { disableLosslessIntegers: true });
 
 module.exports = driver;
