c60bec8a5c101c8ad003126cbd20ea3810932f1f
Folleach <32067915+Folleach@users.noreply.github.com>
2020-12-19 12:42:24 +0500

    Fix bug apply is not a function in async useEffect

--- client/src/pages/EditorPage.js
+++ client/src/pages/EditorPage.js
@@ -1,30 +1,30 @@
 import React, { useEffect } from 'react';
-import ToolButton from '../components/ToolButton';
 import { useAccount } from '../contexts/AccountContext';
-import { useNavigation } from '../contexts/NavigationContext';
 import history from '../history';
 import Editor from '../components/Editor';
-import { Button } from '@material-ui/core';
 import { useAppEditor } from '../contexts/EditorContext';
 
 function EditorPage() {
     const { account } = useAccount();
     const { marks, setMarks, nodeProperties, setNodeProperties, addNodes } = useAppEditor();
 
-    useEffect(async () => {
-        if (!account)
+    useEffect(() => {
+        async function inner() {
+            if (!account)
             return;
-        let marksInfo = await (await fetch(`http://localhost/api/graph/getMarksInfo?token=${account.token}`)).json();
-        const arr = marksInfo.data.response;
-        let map = marks.container;
-        for (let i in arr)
-            map.set(arr[i].type, arr[i].properties);
-        setMarks({container: map});
+            let marksInfo = await (await fetch(`http://localhost/api/graph/getMarksInfo?token=${account.token}`)).json();
+            const arr = marksInfo.data.response;
+            let map = marks.container;
+            for (let i in arr)
+                map.set(arr[i].type, arr[i].properties);
+            setMarks({container: map});
 
-        const nodes = await (await fetch(`http://localhost/api/graph/getNodes?token=${account.token}`)).json();
-        map = nodeProperties.container;
-        addNodes(nodes.data.response);
-        setNodeProperties({container: map});
+            const nodes = await (await fetch(`http://localhost/api/graph/getNodes?token=${account.token}`)).json();
+            map = nodeProperties.container;
+            addNodes(nodes.data.response);
+            setNodeProperties({container: map});
+        }
+        inner();
     }, []);
 
     if (!account)
